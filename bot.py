746a45a3dc3df875d1ff323cefb1975d474e400f
Harry Di <harry_di@hotmail.co.uk>
2023-04-17 00:27:59 +0100

    Refactor pause & el getter, tried signal/threading

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-from selenium.common import TimeoutException
+from selenium.common import TimeoutException, NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 import undetected_chromedriver as uc
@@ -6,6 +6,8 @@ import time
 import os
 import glob
 import traceback
+import threading
+import signal
 import logging
 from logging.handlers import RotatingFileHandler
 
@@ -18,8 +20,7 @@ def sleep_while_browser_alive(code, browser):
             _ = browser.window_handles
             time.sleep(1)
         except Exception as exc:
-            logger.debug(code + "a - " + type(exc).__name__ + ": " + "Browser is no longer alive")
-            logger.error(code + "a - " + traceback.format_exc())
+            log_err(exc, code + "a", "Browser is no longer alive")
             continue_app = False
 
 
@@ -51,15 +52,72 @@ class Fragile(object):
 
 
 def close_and_kill(browser):
+    global kill_thread
     logger.debug("Killing process...")
+    kill_thread = True
     browser.quit()
     raise Fragile.Break
 
 
+def try_screenshot(code, message, browser):
+    try:
+        logger.debug(code + "S - Now taking screenshot for msg: " + message + "...")
+        browser.save_screenshot("screenshots/" + time.strftime("%Y.%m.%d - %H.%M.%S - ") + code + ".png")
+        logger.debug(code + "S1 - Screenshot captured for msg: " + message)
+    except SubThreadException:
+        raise SubThreadException
+    except Exception as exc:
+        log_err(exc, code + "S2", "Failed to take screenshot for msg: " + message)
+
+
+def log_err(error, code, message):
+    logger.debug(code + " - " + type(error).__name__ + ": " + message)
+    logger.error(code + " - " + traceback.format_exc())
+
+
 def handle_err_and_quit(error, code, message, browser):
+    # try_screenshot(code, message, browser)
     logger.debug(code + " - " + type(error).__name__ + ": " + message)
     logger.error(code + " - " + traceback.format_exc())
     close_and_kill(browser)
+
+
+def pause_if_captcha_page_watcher(browser):
+    while True:
+        if kill_thread:
+            return
+        time.sleep(1)
+        logger.debug(captcha_code_1 + " - Checking if captcha page...")
+        if is_el_found_by_attribute_single_check("error-content", captcha_code_1, By.CLASS_NAME, browser):
+            os.kill(os.getpid(), signal.SIGUSR1)  # Throw SubThreadException in main thread via signal
+            logger.debug(captcha_code_2 + "a - Confirmed on captcha page. Waiting for user to continue execution...")
+            input("Press enter to continue execution when page is ready...")
+
+            logger.debug(captcha_code_3 + " - Continuing execution...")
+        else:
+            logger.debug(captcha_code_3 + "b - Not on captcha page. Continuing execution...")
+
+
+def set_captcha_codes(code1, code2, code3):
+    global captcha_code_1, captcha_code_2, captcha_code_3
+    captcha_code_1 = code1
+    captcha_code_2 = code2
+    captcha_code_3 = code3
+
+
+def is_el_found_by_attribute_single_check(name, code, by_attr, browser):
+    try:
+        logger.debug(code + " - Now capturing element by " + str(by_attr) + "...")
+        element = browser.find_element(by=by_attr, value=name)
+        logger.debug(code + "a - Element captured by " + str(by_attr) + ". Element is: " + str(element))
+        return True
+    except NoSuchElementException as exc:
+        logger.error(code + "b" + " - " + type(exc).__name__ + ": " + "Element was not found by " + str(by_attr))
+        return False
+    except SubThreadException:
+        raise SubThreadException
+    except Exception as exc:
+        handle_err_and_quit(exc, code + "c", "Failed to capture element by " + str(by_attr), browser)
 
 
 def try_get_url(url, code, browser):
@@ -67,8 +125,10 @@ def try_get_url(url, code, browser):
         logger.debug(code + " - Now reaching target URL...")
         browser.get(url)
         logger.debug(code + "a - Successfully reached target URL")
+    except SubThreadException:
+        raise SubThreadException
     except Exception as exc:
-        handle_err_and_quit(exc, code, "Failed to reach target URL", browser)
+        handle_err_and_quit(exc, code + "b", "Failed to reach target URL", browser)
 
 
 def quit_if_title_mismatch(title, code1, code2, browser):
@@ -76,6 +136,8 @@ def quit_if_title_mismatch(title, code1, code2, browser):
         logger.debug(code1 + " - Now getting page title...")
         get_title = browser.title
         logger.debug(code1 + "a - Successfully got page title")
+    except SubThreadException:
+        raise SubThreadException
     except Exception as exc:
         handle_err_and_quit(exc, code1 + "b", "Failed to get page title", browser)
 
@@ -88,20 +150,39 @@ def quit_if_title_mismatch(title, code1, code2, browser):
     except ValueError as exc:
         handle_err_and_quit(exc, code2 + "b", "Title not as expected. Expected: " +
                             title + " || Actual: " + get_title, browser)
+    except SubThreadException:
+        raise SubThreadException
     except Exception as exc:
         handle_err_and_quit(exc, code2 + "c", "Failed to check if page title matches", browser)
 
 
-def get_el_by_name_else_quit(name, code, browser, timeout=5):
+def is_el_found_by_attribute(name, code, by_attr, browser, timeout=5, polling=0.5):
     try:
-        logger.debug(code + " - Now capturing element by name...")
-        element = WebDriverWait(browser, timeout=timeout).until(lambda d: d.find_element(by=By.NAME, value=name))
-        logger.debug(code + "a - Element captured by name. Element is: " + str(element))
+        logger.debug(code + " - Now capturing element by " + str(by_attr) + "...")
+        element = WebDriverWait(browser, timeout, polling).until(lambda d: d.find_element(by=by_attr, value=name))
+        logger.debug(code + "a - Element captured by " + str(by_attr) + ". Element is: " + str(element))
+        return True
+    except TimeoutException as exc:
+        log_err(exc, code + "b", "Element was not found by " + str(by_attr))
+        return False
+    except SubThreadException:
+        raise SubThreadException
+    except Exception as exc:
+        handle_err_and_quit(exc, code + "c", "Failed to capture element by " + str(by_attr), browser)
+
+
+def get_el_by_attribute_else_quit(name, code, by_attr, browser, timeout=5, polling=0.5):
+    try:
+        logger.debug(code + " - Now capturing element by " + str(by_attr) + "...")
+        element = WebDriverWait(browser, timeout, polling).until(lambda d: d.find_element(by=by_attr, value=name))
+        logger.debug(code + "a - Element captured by " + str(by_attr) + ". Element is: " + str(element))
         return element
     except TimeoutException as exc:
-        handle_err_and_quit(exc, code + "b", "Element was not found by name", browser)
+        handle_err_and_quit(exc, code + "b", "Element was not found by " + str(by_attr), browser)
+    except SubThreadException:
+        raise SubThreadException
     except Exception as exc:
-        handle_err_and_quit(exc, code + "c", "Failed to capture element by name", browser)
+        handle_err_and_quit(exc, code + "c", "Failed to capture element by " + str(by_attr), browser)
 
 
 def verify_submit_btn_el_else_quit(element, code, browser):
@@ -113,18 +194,61 @@ def verify_submit_btn_el_else_quit(element, code, browser):
             raise ValueError()
     except ValueError as exc:
         handle_err_and_quit(exc, code + "b", "Element is not submit btn as expected.", browser)
+    except SubThreadException:
+        raise SubThreadException
     except Exception as exc:
         handle_err_and_quit(exc, code + "c", "Failed to verify submit btn element", browser)
 
 
-def click_btn_else_quit(element, code, browser):
+def click_el_else_quit(name, element, code, browser):
     try:
-        logger.debug(code + " - Now clicking on button...")
+        logger.debug(code + " - Now clicking on " + name + " element...")
         element.click()
-        logger.debug(code + "a - Successfully clicked on button")
+        logger.debug(code + "a - Successfully clicked on " + name + " element")
+    except SubThreadException:
+        raise SubThreadException
     except Exception as exc:
-        handle_err_and_quit(exc, code + "b", "Failed to click on button", browser)
+        handle_err_and_quit(exc, code + "b", "Failed to click on " + name + " element", browser)
 
+
+def fill_text_input_else_quit(name, element, txt_input, code, browser):
+    try:
+        logger.debug(code + " - Now filling in " + name + " text field...")
+        element.send_keys(txt_input)
+        logger.debug(code + "a - Successfully clicked on " + name + " element")
+    except SubThreadException:
+        raise SubThreadException
+    except Exception as exc:
+        handle_err_and_quit(exc, code + "b", "Failed to fill in " + name + " text field", browser)
+
+
+def try_catch_sub_thread_exc(retry_count, browser, fun, *args, **kwargs):
+    try:
+        result = fun(*args, **kwargs)
+        return result
+    except SubThreadException as exc:
+        logger.debug("444 - " + str(exc) + ": Paused main thread. Waiting for user to continue execution...")
+        input("Press enter to continue main thread execution when page is ready...")
+        logger.debug("444 - Continuing execution...")
+        if retry_count > 0:
+            retry_count -= 1
+            logger.debug("444 - Retrying last function executed. Retries remaining: " + retry_count)
+            try_catch_sub_thread_exc(retry_count, fun, *args, **kwargs)  # If captcha page is resolved, retry last call
+        else:
+            handle_err_and_quit(exc, "444", "No retries left on last function executed. Quitting...", browser)
+    except Fragile.Break:
+        raise Fragile.Break
+
+
+class SubThreadException(Exception):
+    pass
+
+
+def usr1_handler():
+    raise SubThreadException()
+
+
+signal.signal(signal.SIGUSR1, usr1_handler)
 
 file_time = time.strftime("%Y.%m.%d - %H.%M.%S")
 logger = logging.getLogger("Rotating Log")
@@ -136,6 +260,7 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 target_url = "https://driverpracticaltest.dvsa.gov.uk/application"
+license_no = "MORGA657054SM9IJ"
 
 options = uc.ChromeOptions()
 # options.add_argument("--headless")
@@ -145,21 +270,60 @@ options.add_argument("--incognito")
 #                     "Chrome/111.0.0.0 Safari/537.36")
 
 with Fragile(Chrome(options=options, version_main=111)) as driver:
-    try_get_url(target_url, "0.0", driver)
-    input("Press enter to continue execution when page is ready...")
-    # driver.save_screenshot("screenshot1.png")
+    retries = 3
+    try_catch_sub_thread_exc(retries, driver, try_get_url, target_url, "0.00", driver)
 
-    quit_if_title_mismatch("Type of test", "1.1", "1.2", driver)
+    # Step 1 Page
+    captcha_code_1, captcha_code_2, captcha_code_3 = "1.00", "1.01", "1.02"
+    kill_thread = False
+    watcher = threading.Thread(target=pause_if_captcha_page_watcher, args=(driver,))
+    watcher.start()
+    time.sleep(2)  # Wait for sub-thread to check page
 
-    step_one_btn = get_el_by_name_else_quit("testTypeCar", "1.3", driver)
+    try_catch_sub_thread_exc(retries, driver, quit_if_title_mismatch, "Type of test", "1.10", "1.11", driver)
 
-    verify_submit_btn_el_else_quit(step_one_btn, "1.4", driver)
+    step_one_btn = try_catch_sub_thread_exc(retries, driver,
+                                            get_el_by_attribute_else_quit, "testTypeCar", "1.20", By.NAME, driver)
 
-    click_btn_else_quit(step_one_btn, "1.5", driver)
+    try_catch_sub_thread_exc(retries, driver, verify_submit_btn_el_else_quit, step_one_btn, "1.21", driver)
+
+    try_catch_sub_thread_exc(retries, driver, click_el_else_quit, "Submit Btn", step_one_btn, "1.22", driver)
+
+    # Step 2 Page
+    set_captcha_codes("2.00", "2.01", "2.02")
+
+    try_catch_sub_thread_exc(retries, driver, quit_if_title_mismatch, "License details", "2.10", "2.11", driver)
+
+    step_two_license_form = try_catch_sub_thread_exc(
+        retries, driver, get_el_by_attribute_else_quit, "driverLicenceNumber", "2.20", By.NAME, driver)
+
+    try_catch_sub_thread_exc(retries, driver,
+                             click_el_else_quit, "License Input", step_two_license_form, "2.21", driver)
+
+    try_catch_sub_thread_exc(retries, driver,
+                             fill_text_input_else_quit, "License Input", step_two_license_form,
+                             license_no, "2.22", driver)
+
+    step_two_special_needs_form = try_catch_sub_thread_exc(
+        retries, driver, get_el_by_attribute_else_quit, "special-needs-none", "2.30", By.ID, driver)
+
+    try_catch_sub_thread_exc(retries, driver,
+                             click_el_else_quit, "Special Needs Radio Btn", step_two_special_needs_form, "2.31", driver)
+
+    step_two_btn = try_catch_sub_thread_exc(
+        retries, driver, get_el_by_attribute_else_quit, "drivingLicenceSubmit", "2.32", By.NAME, driver)
+
+    try_catch_sub_thread_exc(retries, driver, verify_submit_btn_el_else_quit, step_two_btn, "2.40", driver)
+
+    try_catch_sub_thread_exc(retries, driver, click_el_else_quit, "Submit Btn", step_two_btn, "2.41", driver)
+
+    # Step 3 Page
+    # TODO
 
     # Quit the browser instance
     sleep_while_browser_alive("999", driver)
     logger.debug("Killing process...")
+    kill_thread = True
     raise Fragile.Break
 
 logger.debug("Killed process.")
